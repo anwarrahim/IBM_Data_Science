59aeb6d416e64ef6cbc9114eab16411db40f51d8
Anwar Rahim <anwarrahim@gmail.com>
2023-05-13 14:21:08 +0500

    update the new files

---
+++ course_4/Lab_task_2/practice.py
@@ -0,0 +1,8 @@
+# Import Matplotlib, pandas, and plotly
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
